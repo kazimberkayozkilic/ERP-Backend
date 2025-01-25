be4b4b7c23686299649a239c845e93b8d618885b
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-25 04:30:02 +0300

    feat: Added Customer entity to the domain layer. - Created class `Customer` in the namespace `ERP.Backend.Domain.Entities`. - It inherits from `Entity` to take advantage of shared abstractions. - Defined properties: `Name`, `TaxDepartment`, `TaxNumber`, `City`, `Town` and `FullAddress` with default empty string values.

---
+++ ERP.Backend/ERP.Backend.Domain/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Backend.Domain.Abstractions;
+
+namespace ERP.Backend.Domain.Entities
+{
+    public class Customer: Entity
+    {
+        public string Name { get; set; }= string.Empty;
+        public string TaxDepartment { get; set; }= string.Empty;
+        public string TaxNumber { get; set; }= string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Town { get; set; } = string.Empty;
+        public string FullAddress { get; set; } = string.Empty;
+    }
+}
