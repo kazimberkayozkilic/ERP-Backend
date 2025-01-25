f0fc4f72100a5b4607ae477339bc05625c8e1711
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-25 04:45:50 +0300

    feat: Implemented CustomerRepository for database operations. - The `CustomerRepository` class was created that extends `Repository<Customer, ApplicationDbContext>` and implements `ICustomerRepository`. - It provides database operations for the `Customer` entity using `ApplicationDbContext`.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Repositories
+{
+    internal sealed class CustomerRepository : Repository<Customer, ApplicationDbContext>, ICustomerRepository
+    {
+        public CustomerRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
