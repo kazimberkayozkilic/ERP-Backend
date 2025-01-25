fb9b27f7ad029b316b64b8ddc5f639cfdc807f57
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-25 05:05:12 +0300

    feat: Implemented the GetAllCustomer query handler. - Created `GetAllCustomerQueryHandler` to perform customer fetching from the database.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Customers/GetAllCustomer/GetAllCustomerQuery.cs
@@ -0,0 +1,13 @@
+﻿using ERP.Backend.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Customers.GetAllCustomer
+{
+    public sealed record GetAllCustomerQuery() : IRequest<Result<List<Customer>>>;
+}
---
+++ ERP.Backend/ERP.Backend.Application/Features/Customers/GetAllCustomer/GetAllCustomerQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Customers.GetAllCustomer
+{
+    internal sealed class GetAllCustomerQueryHandler(
+    ICustomerRepository customerRepository) : IRequestHandler<GetAllCustomerQuery, Result<List<Customer>>>
+    {
+        public async Task<Result<List<Customer>>> Handle(GetAllCustomerQuery request, CancellationToken cancellationToken)
+        {
+            List<Customer> customers = await customerRepository.GetAll().OrderBy(p => p.Name).ToListAsync(cancellationToken);
+
+            return customers;
+        }
+    }
+}
