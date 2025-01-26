ff3363484f485961a62aba044c66e7c1e433ae5e
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-26 04:27:39 +0300

    feat: Implemented handler to create a customer. - Added `CreateCustomerCommandHandler` to handle customer creation logic. - Checks if the VAT number already exists in the database before creating a new customer. - Maps `CreateCustomerCommand` to `Customer` using AutoMapper. - Saves the new customer in the database and returns a success message on completion.

--- ERP.Backend/ERP.Backend.Application/Features/Customers/CreateCustomer/CreateCustomerCommand.cs
+++ ERP.Backend/ERP.Backend.Application/Features/Customers/CreateCustomer/CreateCustomerCommand.cs
@@ -1,8 +1,4 @@
-﻿using AutoMapper;
-using ERP.Backend.Domain.Entities;
-using ERP.Backend.Domain.Repositories;
-using GenericRepository;
-using MediatR;
+﻿using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +9,6 @@ using TS.Result;
 namespace ERP.Backend.Application.Features.Customers.CreateCustomer
 {
     public sealed record CreateCustomerCommand(string Name, string TaxDepartment, string TaxNumber, string City, string Town, string FullAddress) : IRequest<Result<string>>;
-  
-    internal sealed class CreateCustomerCommandHandler(ICustomerRepository customerRepository, IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<CreateCustomerCommand, Result<string>>
-    {
-        public async Task<Result<string>> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
-        {
-            bool isTaxNumberExists = await customerRepository.AnyAsync(p => p.TaxNumber == request.TaxNumber, cancellationToken);
-            if (isTaxNumberExists)
-            {
-                return Result<string>.Failure("Vat number already exists");
-            }
-            Customer customer = mapper.Map<Customer>(request);
-            await customerRepository.AddAsync(customer, cancellationToken);
-            await unitOfWork.SaveChangesAsync(cancellationToken);
-            return "Customer created successfully";
-        }
-    }
 }
 
 
---
+++ ERP.Backend/ERP.Backend.Application/Features/Customers/CreateCustomer/CreateCustomerCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Customers.CreateCustomer
+{
+    internal sealed class CreateCustomerCommandHandler(ICustomerRepository customerRepository, IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<CreateCustomerCommand, Result<string>>
+    {
+        public async Task<Result<string>> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            bool isTaxNumberExists = await customerRepository.AnyAsync(p => p.TaxNumber == request.TaxNumber, cancellationToken);
+            if (isTaxNumberExists)
+            {
+                return Result<string>.Failure("Vat number already exists");
+            }
+            Customer customer = mapper.Map<Customer>(request);
+            await customerRepository.AddAsync(customer, cancellationToken);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return "Customer created successfully";
+        }
+    }
+}
+
+
