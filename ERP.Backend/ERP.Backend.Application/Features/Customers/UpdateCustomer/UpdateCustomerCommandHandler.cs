4a3ff9879a093f63b24346443a4480e6662b0766
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-29 01:58:10 +0300

    feat: Implemented UpdateCustomerCommandHandler for customer updates. - Created `UpdateCustomerCommandHandler` to handle the customer update logic.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Customers/UpdateCustomer/UpdateCustomerCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Customers.UpdateCustomer
+{
+    internal sealed class UpdateCustomerCommandHandler( ICustomerRepository customerRepository, IUnitOfWork unitOfWork, IMapper mapper )
+        : IRequestHandler<UpdateCustomerCommand, Result<string>>
+    {
+        public async Task<Result<string>> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            Customer customer = await customerRepository.GetByExpressionWithTrackingAsync(p => p.Id == request.Id, cancellationToken);
+            if (customer is null)
+            {
+                return Result<string>.Failure("Müşteri Bulunamadı");
+            }
+            if(customer.TaxNumber != request.TaxNumber)
+            {
+                bool isTaxNumberExists = await customerRepository.AnyAsync(p => p.TaxNumber == request.TaxNumber, cancellationToken);
+                if (isTaxNumberExists)
+                {
+                    return Result<string>.Failure("Vat number already exists");
+                }
+            }
+            mapper.Map(request, customer);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return "Müşteri başarıyla güncellendi";
+        }
+    }
+}
