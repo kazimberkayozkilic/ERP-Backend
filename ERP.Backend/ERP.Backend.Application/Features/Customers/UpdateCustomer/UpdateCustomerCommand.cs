29f7b96762270b820ebe6a5acc4d1844b3953321
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-29 01:42:49 +0300

    Implemented UpdateCustomerCommand and UpdateCustomerCommandHandler to update customer information. - UpdateCustomerCommand was created to process customer update requests. - Implemented UpdateCustomerCommandHandler to process the update logic.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Customers/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Customers.UpdateCustomer
+{
+    public sealed record UpdateCustomerCommand(string Name, string TaxDepartment, string TaxNumber, string City, string Town, string FullAddress) : IRequest<Result<string>>;
+   
+}
