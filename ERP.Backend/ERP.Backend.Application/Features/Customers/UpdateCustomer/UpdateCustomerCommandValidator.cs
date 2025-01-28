3a2bdfb192dc98663603d7c79f34634013f37ae2
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-29 02:01:59 +0300

    feat: Added FluentValidation for UpdateCustomerCommand. - Created `UpdateCustomerCommandValidator` using FluentValidation to validate input data.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Customers/UpdateCustomer/UpdateCustomerCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Application.Features.Customers.UpdateCustomer
+{
+    public sealed class UpdateCustomerCommandValidator : AbstractValidator<UpdateCustomerCommand>
+    {
+        public UpdateCustomerCommandValidator()
+        {
+            RuleFor(p => p.Name).MinimumLength(3);
+            RuleFor(p => p.TaxNumber).MinimumLength(10).MaximumLength(11);
+        }
+    }
+}
