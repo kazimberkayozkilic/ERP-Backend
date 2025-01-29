5cfbc19936a28b4c9e4c6302d5b402a5956859fd
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-30 00:43:22 +0300

    feat: Add DepotRepository and created migration mg4. - Implemented `DepotRepository` to manage repository related database operations. - Completed `add-migration mg4` to implement schema changes related to repositories.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/DepotRepository.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Repositories
+{
+    internal sealed class DepotRepository : Repository<Depot, ApplicationDbContext>, IDepotRepository
+    {
+        public DepotRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
