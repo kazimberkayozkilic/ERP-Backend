c884537cfad9fc34d12bd8d0f70cc2ebed4ad849
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-30 03:38:50 +0300

    feat: Added mapping to Depot for UpdateDepotCommand. - Added mapping configuration for `UpdateDepotCommand` to `Depot` in `MappingProfile`.

--- ERP.Backend/ERP.Backend.Application/Mapping/MappingProfile.cs
+++ ERP.Backend/ERP.Backend.Application/Mapping/MappingProfile.cs
@@ -2,6 +2,7 @@
 using ERP.Backend.Application.Features.Customers.CreateCustomer;
 using ERP.Backend.Application.Features.Customers.UpdateCustomer;
 using ERP.Backend.Application.Features.Depots.CreateDepot;
+using ERP.Backend.Application.Features.Depots.UpdateDepot;
 using ERP.Backend.Domain.Entities;
 
 namespace ERP.Backend.Application.Mapping
@@ -13,6 +14,8 @@ namespace ERP.Backend.Application.Mapping
             CreateMap<CreateCustomerCommand, Customer>();
             CreateMap<UpdateCustomerCommand, Customer>();
             CreateMap<CreateDepotCommand, Depot>();
+            CreateMap<UpdateDepotCommand, Depot>();
+
         }
     }
 }
