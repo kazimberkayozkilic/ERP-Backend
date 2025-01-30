546f9bceaf09df0db73b127ab292e18f9982c6b3
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-30 03:41:47 +0300

    feat: Added DepotsController with CRUD operations for the repository. - Implemented `DepotsController` with actions to create, update, delete and retrieve repositories. - It is integrated with MediatR for command and query processing. - Actions include `CreateDepotCommand`, `UpdateDepotCommand`, `DeleteDepotByIdCommand` and `GetAllDepotQuery`.

---
+++ ERP.Backend/ERP.Backend.WebAPI/Controllers/DepotsController.cs
@@ -0,0 +1,50 @@
+﻿using ERP.Backend.Application.Features.Depots.CreateDepot;
+using ERP.Backend.Application.Features.Depots.DeleteDepotById;
+using ERP.Backend.Application.Features.Depots.GetAllDepot;
+using ERP.Backend.Application.Features.Depots.UpdateDepot;
+using ERP.Backend.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Backend.WebAPI.Controllers
+{
+    public sealed class DepotsController : ApiController
+    {
+        public DepotsController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAll(GetAllDepotQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateDepotCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteById(DeleteDepotByIdCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateDepotCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+    }
+}
