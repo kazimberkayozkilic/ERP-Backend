6fd4c18e5a6c74fa9cb879d3f6acd535bfc38796
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-30 03:00:30 +0300

    feat: Added CreateDepotCommandHandler for handling depot creation. - Implemented `CreateDepotCommandHandler` to process depot creation requests. - Uses `IMapper` to map `CreateDepotCommand` to a `Depot` entity. - Saves the new depot using `IDepotRepository` and commits with `IUnitOfWork`. - Returns a success message upon completion.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Depots/CreateDepot/CreateDepotCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Depots.CreateDepot
+{
+    internal sealed class CreateDepotCommandHandler(IDepotRepository depotRepository, IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<CreateDepotCommand, Result<string>>
+    {
+        public async Task<Result<string>> Handle(CreateDepotCommand request, CancellationToken cancellationToken)
+        {
+            Depot depot = mapper.Map<Depot>(request);
+            await depotRepository.AddAsync(depot, cancellationToken);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return"Depo başarıyla oluşturuldu.";
+        }
+    }
+}
