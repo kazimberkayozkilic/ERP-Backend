006b6b9dd4ea15539a1a47ecbce9eec60403d00d
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 02:37:19 +0300

    feat: Implemented DeleteProductByIdCommandHandler for product deletion. - Implemented `DeleteProductByIdCommandHandler` to perform product deletion with `Id`. - Retrieves the product from the warehouse and deletes it if found. - Returns an error message if the product is not found. - Processes the changes using `IUnitOfWork` after the deletion.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Products/DeleteProductById/DeleteProductByIdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Products.DeleteProductById
+{
+    internal sealed class DeleteProductByIdCommandHandler(IProductRepository productRepository, IUnitOfWork unitOfWork) : IRequestHandler<DeleteProductByIdCommand, Result<string>>
+    {
+        public async Task<Result<string>> Handle(DeleteProductByIdCommand request, CancellationToken cancellationToken)
+        {
+            Product product = await productRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+            if (product == null)
+            {
+                return Result<string>.Failure("Ürün bulunamadı.");
+            }
+            productRepository.Delete(product);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return "Ürün başarıyla silindi.";
+        }
+    }
+}
