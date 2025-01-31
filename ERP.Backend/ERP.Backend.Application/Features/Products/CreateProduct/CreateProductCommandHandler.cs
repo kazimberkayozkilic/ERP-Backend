b2fbc49361f1baf5e9c2394286c531300e0a4b9b
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 02:24:30 +0300

    feat: Added CreateProductCommandHandler to handle product creation. - Implemented `CreateProductCommandHandler` to handle product creation requests. - It checks for duplicate product names before creating a new product. - Uses `AutoMapper` to map the CreateProductCommand to a `Product` entity. - Adds the new product to the repository and processes the changes with `IUnitOfWork`.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Products.CreateProduct
+{
+    internal sealed class CreateProductCommandHandler(IProductRepository productRepository, IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<CreateProductCommand, Result<string>>
+    {
+        public async Task<Result<string>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            bool isNameExists = await productRepository.AnyAsync(p => p.Name == request.Name, cancellationToken);
+            if (isNameExists)
+            {
+                return Result<string>.Failure("Ürün adı zaten mevcut");
+            }
+            Product product = mapper.Map<Product>(request);
+            await productRepository.AddAsync(product, cancellationToken);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return "Ürün eklendi";
+        }
+    }
+}
