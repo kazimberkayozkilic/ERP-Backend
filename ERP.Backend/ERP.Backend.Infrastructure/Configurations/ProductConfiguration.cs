363b7b610d46ea077819d00c54a99cb2ec099246
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 01:52:08 +0300

    feat: Added ProductConfiguration for Entity Framework. - Implemented `ProductConfiguration` to configure the `Product` entity. - The `Type` property has been configured to use value transformation with `ProductTypeEnum`. - It ensures correct mapping of `SmartEnum` values in EF Core.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Configurations
+{
+    internal sealed class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        void IEntityTypeConfiguration<Product>.Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Type).HasConversion(type => type.Value, value =>ProductTypeEnum.FromValue(value));
+        }
+    }
+}
