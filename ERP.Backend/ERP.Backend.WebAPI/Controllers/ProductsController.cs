a3356a76cbef1c4c60ab8ff623ce6bccf1b70d76
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 02:52:29 +0300

    feat: Implemented ProductsController with CRUD actions. - Created `ProductsController` to handle product related requests.

---
+++ ERP.Backend/ERP.Backend.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using ERP.Backend.Application.Features.Depots.UpdateDepot;
+using ERP.Backend.Application.Features.Products.CreateProduct;
+using ERP.Backend.Application.Features.Products.DeleteProductById;
+using ERP.Backend.Application.Features.Products.GetAllProduct;
+using ERP.Backend.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Backend.WebAPI.Controllers
+{
+    public sealed class ProductsController : ApiController
+    {
+        public ProductsController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAll(GetAllProductQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteById(DeleteProductByIdCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateDepotCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+    }
+}
