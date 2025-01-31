3dbe7aba91aead2c391d82fd3270502f705cc548
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 02:57:57 +0300

    feat: Added CreateProductCommandValidator for product validation. - Implemented `CreateProductCommandValidator` using FluentValidation to provide validation of product creation. - Added validation rule to check if the value of `TypeValue` is greater than 0.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Products/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Application.Features.Products.CreateProduct
+{
+    public sealed class CreateProductCommandValidator: AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator() 
+        {
+            RuleFor(p => p.TypeValue).GreaterThan(0);
+        }
+    }
+}
