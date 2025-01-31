17ab8f7a78e0f2f8ab6ebcf5580d00bad97ceb9d
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 01:55:24 +0300

    feat: ProductRepository has been implemented. - Created `ProductRepository` which implements `IProductRepository`. - Inherits from `Repository<Product, ApplicationDbContext>` for general repository functionality. - Enables database operations for `Product` entities.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Configurations;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Repositories
+{
+    internal sealed class ProductRepository : Repository<Product, ApplicationDbContext>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
