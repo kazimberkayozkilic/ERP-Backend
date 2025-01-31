a750704edc1907d04027fbce65041bc4a0f3c5e1
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 02:05:34 +0300

    feat: Implemented `GetAllQueryHandler` to get the product list. - Created `GetAllQueryHandler` to handle `GetAllProductQuery` requests. - Injects `IProductRepository` to fetch all products.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Products/GetAllProduct/GetAllQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Products.GetAllProduct
+{
+    internal sealed class GetAllQueryHandler(IProductRepository productRepository) : IRequestHandler<GetAllProductQuery, Result<List<Product>>>
+    {
+        public async Task<Result<List<Product>>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
+        {
+            List<Product> products = await productRepository.GetAll().OrderBy(p => p.Name).ToListAsync(cancellationToken);
+            return products;
+        }
+    }
+}
