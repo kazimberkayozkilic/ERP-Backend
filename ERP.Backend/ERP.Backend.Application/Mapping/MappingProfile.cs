2db2c9746adafad662b49781eb44c7472fcba5a7
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 02:29:10 +0300

    feat: Added AutoMapper configuration for CreateProductCommand. - Added `CreateMap<CreateProductCommand, Product>` to map CreateProductCommand to `Product`. - Configured `Type` property mapping to convert `TypeValue` property to the corresponding `ProductTypeEnum` value using `FromValue`.

--- ERP.Backend/ERP.Backend.Application/Mapping/MappingProfile.cs
+++ ERP.Backend/ERP.Backend.Application/Mapping/MappingProfile.cs
@@ -3,7 +3,9 @@ using ERP.Backend.Application.Features.Customers.CreateCustomer;
 using ERP.Backend.Application.Features.Customers.UpdateCustomer;
 using ERP.Backend.Application.Features.Depots.CreateDepot;
 using ERP.Backend.Application.Features.Depots.UpdateDepot;
+using ERP.Backend.Application.Features.Products.CreateProduct;
 using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Enums;
 
 namespace ERP.Backend.Application.Mapping
 {
@@ -15,6 +17,8 @@ namespace ERP.Backend.Application.Mapping
             CreateMap<UpdateCustomerCommand, Customer>();
             CreateMap<CreateDepotCommand, Depot>();
             CreateMap<UpdateDepotCommand, Depot>();
+            CreateMap<CreateProductCommand, Product>().ForMember(member => member.Type, options => options.MapFrom(p => ProductTypeEnum.FromValue(p.TypeValue)));
+
 
         }
     }
