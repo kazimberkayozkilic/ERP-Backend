503d08cb2e4f7285680373d73542dea50da93e01
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 01:44:40 +0300

    feat: Added Product Entity and ProductTypeEnum. - Created `Product` entity with `Name` and `Type` properties. - For better type safety, `ProductTypeEnum` was introduced using `Ardalis.SmartEnum`. - Product` and `SemiProduct` are defined as enum values.

---
+++ ERP.Backend/ERP.Backend.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using ERP.Backend.Domain.Abstractions;
+using ERP.Backend.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Domain.Entities
+{
+    public sealed class Product: Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public ProductTypeEnum Type { get; set; } = ProductTypeEnum.Product;
+    }
+}
