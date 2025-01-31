cff829bf66323c649fb9e9a967880ec0dfa1405d
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-01 02:01:40 +0300

    feat: Added GetAllProductQuery to get a list of products. - Created `GetAllProductQuery` to get a list of all products using MediatR.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Products/GetAllProduct/GetAllProductQuery.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Backend.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Products.GetAllProduct
+{
+    public sealed record GetAllProductQuery() : IRequest<Result<List<Product>>>;
+    
+}
