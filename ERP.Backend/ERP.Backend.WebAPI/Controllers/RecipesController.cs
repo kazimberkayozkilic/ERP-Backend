8e85e276d40087b38f4fc82a704fab88067c3156
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 02:57:44 +0300

    feat: Added RecipesController to handle recipe related requests. - Implemented `RecipesController` which contains actions to retrieve, create and delete recipes. - It uses MediatR for communication with the application layer.

---
+++ ERP.Backend/ERP.Backend.WebAPI/Controllers/RecipesController.cs
@@ -0,0 +1,44 @@
+﻿using ERP.Backend.Application.Features.Products.CreateProduct;
+using ERP.Backend.Application.Features.Products.DeleteProductById;
+using ERP.Backend.Application.Features.Products.GetAllProduct;
+using ERP.Backend.Application.Features.Products.UpdateProduct;
+using ERP.Backend.Application.Features.Recipes.CreateRecipes;
+using ERP.Backend.Application.Features.Recipes.DeleteRecipeByIdCommand.cs;
+using ERP.Backend.Application.Features.Recipes.GetAllRecipe;
+using ERP.Backend.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Backend.WebAPI.Controllers
+{
+    public sealed class RecipesController : ApiController
+    {
+        public RecipesController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAll(GetAllRecipeQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRecipeCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteById(DeleteRecipeByIdCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
