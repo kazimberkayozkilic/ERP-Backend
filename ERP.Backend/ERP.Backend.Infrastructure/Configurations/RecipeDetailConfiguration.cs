96b83bb1e9afaf3886ab195f6ba16ab6f475f937
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 01:11:35 +0300

    feat: Added RecipeDetailConfiguration for entity mapping. - Implemented `IEntityTypeConfiguration<RecipeDetail>`. - Defined `Configure` method to set entity configurations.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Configurations/RecipeDetailConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Configurations
+{
+    internal sealed class RecipeDetailConfiguration : IEntityTypeConfiguration<RecipeDetail>
+    {
+        public void Configure(EntityTypeBuilder<RecipeDetail> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
