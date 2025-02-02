b89f64af72bb6f5ec5d2292d5af6ab6cb28acee6
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 01:15:41 +0300

    feat: RecipeRepository` is implemented. - A `RecipeRepository` is created by implementing `RecipeRepository`. - It is inherited from `Repository<Recipe, ApplicationDbContext>`. - Enables database operations for the `Recipe` entity.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/RecipeRepository.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Repositories
+{
+    internal sealed class RecipeRepository : Repository<Recipe, ApplicationDbContext>, IRecipeRepository
+    {
+        public RecipeRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
