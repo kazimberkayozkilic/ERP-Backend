e7032e9f1c2c65ee1931ccfbc7636b390f0828f3
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 02:50:42 +0300

    feat: Implemented recipe creation and deletion commands. - Added `CreateRecipeCommandHandler` to handle recipe creation with validation. - Added `DeleteRecipeByIdCommand` to request deletion of a recipe by its ID. - Both commands use MediatR to handle requests and results.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Recipes/CreateRecipes/CreateRecipeCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Recipes.CreateRecipes
+{
+    internal sealed class CreateRecipeCommandHandler(IRecipeRepository recipeRepository, IUnitOfWork unitOfWork) : IRequestHandler<CreateRecipeCommand, Result<string>>
+    {
+        public async Task<Result<string>> Handle(CreateRecipeCommand request, CancellationToken cancellationToken)
+        {
+            bool isRecipeExists = await recipeRepository.AnyAsync(p => p.ProductId == request.ProductId, cancellationToken);
+            if (isRecipeExists)
+            {
+                return Result<string>.Failure("Bu ürün için bir tarım tarifi bulunmaktadır.");
+            }
+            Recipe recipe = new()
+            {
+                ProductId = request.ProductId,
+                RecipeDetails = request.Details.Select(p => new RecipeDetail()
+                {
+                    ProductId = p.ProductId,
+                    Quantity = p.Quantity
+                }).ToList()
+            };
+
+            await recipeRepository.AddAsync(recipe);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return "Reçete kaydı başarıyla oluşturuldu.";
+        }
+    }
+}
---
+++ ERP.Backend/ERP.Backend.Application/Features/Recipes/DeleteRecipeByIdCommand.cs/DEleteRecipeByIdCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Recipes.DeleteRecipeByIdCommand.cs
+{
+    public sealed record DeleteRecipeByIdCommand(Guid Id) : IRequest<Result<string>>;
+    
+}
