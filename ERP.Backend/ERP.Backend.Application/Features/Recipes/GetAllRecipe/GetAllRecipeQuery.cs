b92e1c408dd1ce658897f3102d42c48f93b5a60f
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 02:23:43 +0300

    chore: added `GetAllRecipeQuery` to get all recipes. - A new query `GetAllRecipeQuery` was created to get all recipes. - `Result<List<Recipe>> was used for response processing. - Integrated `IRequest` from MediatR for query based communication.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Recipes/GetAllRecipe/GetAllRecipeQuery.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Backend.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Recipes.GetAllRecipe
+{
+    public sealed record GetAllRecipeQuery(): IRequest<Result<List<Recipe>>>;
+    
+}
