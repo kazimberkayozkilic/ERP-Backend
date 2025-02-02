04ad907efb2e27a038842109366968db6c8bf411
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 01:09:53 +0300

    feat: Added RecipeConfiguration for Entity Framework. - IntityTypeConfiguration<Recipe>` has been implemented. - Added `RecipeConfiguration` to define database mappings.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Configurations/RecipeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Configurations
+{
+    internal sealed class RecipeConfiguration : IEntityTypeConfiguration<Recipe>
+    {
+        public void Configure(EntityTypeBuilder<Recipe> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
