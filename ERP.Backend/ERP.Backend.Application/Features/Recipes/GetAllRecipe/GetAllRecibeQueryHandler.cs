1a2b1df45725d9fa621d15e173ccebff0cbb2432
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 02:28:33 +0300

    feat: implemented `GetAllRecipeQueryHandler` to handle recipe retrieval. - Created `GetAllRecipeQueryHandler` to handle the query `GetAllRecipeQueryQuery`. - Implemented logic to retrieve all recipes sorted by product name with their associated products. - Used `Include` to load the relevant `Product` data and `ToListAsync` for asynchronous database query.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Recipes/GetAllRecipe/GetAllRecibeQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Recipes.GetAllRecipe
+{
+    internal sealed class GetAllRecibeQueryHandler(IRecipeRepository recipeRepository) : IRequestHandler<GetAllRecipeQuery, Result<List<Recipe>>>
+    {
+        public async Task<Result<List<Recipe>>> Handle(GetAllRecipeQuery request, CancellationToken cancellationToken)
+        {
+            List<Recipe> recipes = await recipeRepository.GetAll().Include(p => p.Product).OrderBy(p => p.Product!.Name).ToListAsync(cancellationToken);
+            return recipes;
+        }
+    }
+}
