8aeb730992a53f6c7eb60489f398c6d0e2ac3059
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 01:17:25 +0300

    feat: RecipeDetailRepository is implemented. - RecipeDetailRepository ` is created by implementing `RecipeDetailRepository `. - Inherited from `Repository<RecipeDetail, ApplicationDbContext>`. - Enables database operations for the `Recipe` entity.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/RecipeDetailRepository.cs
@@ -0,0 +1,11 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+
+internal sealed class RecipeDetailRepository : Repository<RecipeDetail, ApplicationDbContext>, IRecipeDetailRepository
+{
+    public RecipeDetailRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
