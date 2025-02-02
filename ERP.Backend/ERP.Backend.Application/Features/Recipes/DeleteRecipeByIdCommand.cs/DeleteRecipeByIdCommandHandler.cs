04727f7496cb112c2c8742f44a0802cd1fe61e91
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 02:55:01 +0300

    feat: Implemented the recipe deletion command handler. - Added `DeleteRecipeByIdCommandHandler` to perform recipe deletion by ID. - It uses MediatR for request processing and the repository model for deleting recipes.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Recipes/DeleteRecipeByIdCommand.cs/DeleteRecipeByIdCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Recipes.DeleteRecipeByIdCommand.cs
+{
+    internal sealed class DeleteRecipeByIdCommandHandler(IRecipeRepository recipeRepository, IUnitOfWork unitOfWork) : IRequestHandler<DeleteRecipeByIdCommand, Result<string>>
+    {
+        public async Task<Result<string>> Handle(DeleteRecipeByIdCommand request, CancellationToken cancellationToken)
+        {
+            Recipe recipe = await recipeRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+            recipeRepository.Delete(recipe);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return "Reçete başarıyla silindi.";
+        }
+    }
+}
