e626cc383df3e56522ef07c478a0833f836db77d
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-03 02:21:08 +0300

    chore: Added `Product` property to `Recipe` entity. - Added nullable `Product` property to `Recipe` element to establish a relationship with `Product` element. - Improved the `Recipe` class to support a collection of `RecipeDetail` elements.

--- ERP.Backend/ERP.Backend.Domain/Entities/Recipe.cs
+++ ERP.Backend/ERP.Backend.Domain/Entities/Recipe.cs
@@ -10,6 +10,7 @@ namespace ERP.Backend.Domain.Entities
     public sealed class Recipe: Entity
     {
         public Guid ProductId { get; set; }
+        public Product? Product { get; set; }
         public List<RecipeDetail>? RecipeDetails { get; set; }
     }
 }
