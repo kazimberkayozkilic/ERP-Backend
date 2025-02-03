36a9415ef28699ea5baa162a2e2ca34b88e95275
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-04 02:10:37 +0300

    feat: UpdateRecipeDetailCommand added.- - Created `UpdateRecipeDetailCommand` to update recipe detail entries. - It contains fields for `Id`, `ProductId` and `Quantity`. - Uses MediatR to process update requests.

---
+++ ERP.Backend/ERP.Backend.Application/Features/RecipeDetails/UpdateRecipeDetail/UpdateRecipeDetailCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.RecipeDetails.UpdateRecipeDetail
+{
+    public sealed record UpdateRecipeDetailCommand(Guid Id, Guid ProductId, decimal Quantity):IRequest<Result<string>>;
+}
