48a08e7727ccb6518a5cc987183d831bb3854b2e
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-04 01:54:28 +0300

    feat: Implemented the `DeleteRecipeDetailByIdCommand` command. - Added `DeleteRecipeDetailByIdCommand` to handle deletion of a recipe detail by id.

---
+++ ERP.Backend/ERP.Backend.Application/Features/RecipeDetails/DeleteRecipeDetailById/DeleteRecipeDetailByIdCommad.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.RecipeDetails.DeleteRecipeDetailById
+{
+    public sealed record DeleteRecipeDetailByIdCommad(Guid Id) : IRequest<Result<String>>;
+    
+}
