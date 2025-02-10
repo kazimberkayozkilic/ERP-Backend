ed5a93557bfe8d7592f8decf883ef4305b333fa6
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-10 18:35:54 +0300

    feat: OrderDetail entity added. - Created `OrderDetail` entity with order, product, quantity and price properties. - Relationships were established with `Order` and `Product` entities.

---
+++ ERP.Backend/ERP.Backend.Domain/Entities/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using ERP.Backend.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Domain.Entities
+{
+    public sealed class OrderDetail : Entity
+    {
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public Product? Product { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
