6d342924b60b6197ab089a22c2734ba46b7dbf62
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-11 02:03:53 +0300

    feat: Implemented OrdersController with CRUD operations. - OrdersController` class was created and the following methods were added for basic CRUD operations:   - `GetAll`: Method to get all orders.   - `Create`: Method to create a new order.   - `DeleteById`: Method to delete an order by ID.   - `Update`: Method that updates an existing order. - All methods use `IMediator` to process the related commands.

---
+++ ERP.Backend/ERP.Backend.WebAPI/Controllers/OrdersController.cs
@@ -0,0 +1,54 @@
+﻿using ERP.Backend.Application.Features.Customers.CreateCustomer;
+using ERP.Backend.Application.Features.Customers.DeleteCustomerById;
+using ERP.Backend.Application.Features.Customers.GetAllCustomer;
+using ERP.Backend.Application.Features.Customers.UpdateCustomer;
+using ERP.Backend.Application.Features.Orders.CreateOrder;
+using ERP.Backend.Application.Features.Orders.DeleteOrder;
+using ERP.Backend.Application.Features.Orders.GetAllOrder;
+using ERP.Backend.Application.Features.Orders.UpdateOrder;
+using ERP.Backend.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Backend.WebAPI.Controllers
+{
+    public sealed class OrdersController : ApiController
+    {
+        public OrdersController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAll(GetAllOrderQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteById(DeleteOrderByIdCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+    }
+}
