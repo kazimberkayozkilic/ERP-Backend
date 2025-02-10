f54bb37531b438c7eba9756d11921a04e3b0c399
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-11 01:01:38 +0300

    feat: CreateOrderCommand and CreateOrderCommandHandler implemented. - The `CreateOrderCommand` class was created and implemented with MediatR. - The necessary parameters for the order creation process were defined. - CreateOrderCommandHandler` class was created and implemented with `IRequestHandler`. - Order creation process was added with business logic.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Orders/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Backend.Domain.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Orders.CreateOrder
+{
+    public sealed record class CreateOrderCommand(Guid CustomerId, DateTime Date, DateTime DeliveryDate, List<OrderDetailDto> Details) : IRequest<Result<string>>;
+    
+}
