6f7c308b309c2caa5c8f1592c768851a24ec76ba
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-11 01:45:48 +0300

    feat: UpdateOrderCommand has been implemented. - The `UpdateOrderCommand` class has been created and the necessary parameters for the order update process have been added. - It contains the properties of the order such as `Id`, `CustomerId`, `Date`, `DeliveryDate` and `Details`.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Orders/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Backend.Domain.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Orders.UpdateOrder
+{
+    public sealed record class UpdateOrderCommand(Guid Id, Guid CustomerId, DateTime Date, DateTime DaliveryDate, List<OrderDetailDto> Details): IRequest<Result<string>>;
+ 
+}
