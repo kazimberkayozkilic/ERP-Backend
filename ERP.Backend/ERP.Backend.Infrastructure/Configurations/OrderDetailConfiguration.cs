033cd75a897bd791708b2725189cf5236d02ee15
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-10 19:14:17 +0300

    feat: Added OrderDetailConfiguration for EF Core. - Created `OrderDetailConfiguration` which implements `IntityTypeConfiguration<OrderDetail>`. - Configured `Product` relationship with `OnDelete(DeleteBehavior.NoAction)`. - Set the column type `Price` to `money`. - Set the column type `Quantity` to `decimal(7,2)`.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Configurations/OrderDetailConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ERP.Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Configurations
+{
+    internal sealed class OrderDetailConfiguration : IEntityTypeConfiguration<OrderDetail>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetail> builder)
+        {
+            builder.HasOne(p => p.Product).WithMany().OnDelete(DeleteBehavior.NoAction);
+            builder.Property(p => p.Price).HasColumnType("money");
+            builder.Property(p => p.Quantity).HasColumnType("decimal(7,2)");
+        }
+    }
+}
