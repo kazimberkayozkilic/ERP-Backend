9c518542ec296decdcebcd5fa7a5303eb0e6ff8b
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-10 19:22:10 +0300

    feat: Implemented OrderRepository. - Created `OrderRepository` by implementing `IOrderReporsitory` to manage orders. - It inherits from `Repository<Order, ApplicationDbContext>` to provide basic CRUD functionality.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Repositories
+{
+    internal sealed class OrderRepository : Repository<Order, ApplicationDbContext>, IOrderReporsitory
+    {
+        public OrderRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
