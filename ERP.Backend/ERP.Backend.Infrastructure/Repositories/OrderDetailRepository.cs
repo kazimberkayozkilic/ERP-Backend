c8c580edbc89abe88c76bcdb60491644e42184a2
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-10 19:30:38 +0300

    feat: OrderDetailRepository Class Created. - Implemented `OrderDetailRepository` class for managing `OrderDetail` entities. - It inherits from the `Repository<OrderDetail>` class and implements the `IOrderDetailRepository` class.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Repositories
+{
+    internal sealed class OrderDetailRepository : Repository<OrderDetail, ApplicationDbContext>, IOrderDetailRepository
+    {
+        public OrderDetailRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
