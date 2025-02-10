255400e9fb6678c73eaef2632bc62984cc698557
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-10 18:32:48 +0300

    feat: Added Order entity and OrderStatusEnum. - Created `Order` entity with properties for customer, order details and status. - Implemented `OrderStatusEnum` using `SmartEnum` for better status management.

---
+++ ERP.Backend/ERP.Backend.Domain/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Backend.Domain.Abstractions;
+using ERP.Backend.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Domain.Entities
+{
+    public sealed class Order : Entity
+    {
+        public Guid CustomerId { get; set; }
+        public Customer? Customer{ get; set; }
+        public string OrderNumber{ get; set; }= string.Empty;
+        public DateTime Date{ get; set; }
+        public DateTime DeliveryDate{ get; set; }
+        public OrderStatüsEnum Status { get; set; } = OrderStatüsEnum.Pending;
+    }
+}
---
+++ ERP.Backend/ERP.Backend.Domain/Enums/OrderStatüsEnum.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.SmartEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Domain.Enums
+{
+    public sealed class OrderStatüsEnum : SmartEnum<OrderStatüsEnum>
+    {
+        public static readonly OrderStatüsEnum Pending = new("Bekliyor" , 1);
+        public static readonly OrderStatüsEnum RequirementsPlanWorked = new("İhtiyaç Planı Çalışıldı", 2);
+        public static readonly OrderStatüsEnum Completed = new("Tamamlandı", 3);
+        public OrderStatüsEnum(string name, int value) : base(name, value)
+        {
+        }
+    }
+}
