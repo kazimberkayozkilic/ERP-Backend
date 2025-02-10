09200126536b0807029a70cedc764fed63a7a1e7
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-10 19:10:04 +0300

    feat: Added OrderConfiguration for EF Core. - Created `OrderConfiguration` which implements `IntityTypeConfiguration<Order>`. - OrderNumber` is configured as `varchar(16)`. - Status` is configured to store enum values using `HasConversion`.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERP.Backend.Infrastructure.Configurations
+{
+    internal sealed class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(p => p.OrderNumber).HasColumnType("varchar(16)");
+            builder.Property(p => p.Status).HasConversion(status => status.Value, value => OrderStatüsEnum.FromValue(value));
+        }
+    }
+}
