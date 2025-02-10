2591cdb6785a87dd15094401372a8b3f55a0cb91
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-11 01:28:17 +0300

    feat: CreateOrderCommand → Order mapping implemented. - Added `CreateMap<CreateOrderCommand, Order>` to AutoMapper profile. - Mapped `ForMember` to `Order.Details` field manually:   - CreateOrderCommand.Details` into an `OrderDetail` object.   - The fields `Price`, `ProductId` and `Quantity` are assigned.   - The result is converted to a list and assigned to the `Order.Details` object.

--- ERP.Backend/ERP.Backend.Application/Mapping/MappingProfile.cs
+++ ERP.Backend/ERP.Backend.Application/Mapping/MappingProfile.cs
@@ -3,6 +3,7 @@ using ERP.Backend.Application.Features.Customers.CreateCustomer;
 using ERP.Backend.Application.Features.Customers.UpdateCustomer;
 using ERP.Backend.Application.Features.Depots.CreateDepot;
 using ERP.Backend.Application.Features.Depots.UpdateDepot;
+using ERP.Backend.Application.Features.Orders.CreateOrder;
 using ERP.Backend.Application.Features.Products.CreateProduct;
 using ERP.Backend.Application.Features.Products.UpdateProduct;
 using ERP.Backend.Application.Features.RecipeDetails.CreateRecipeDetail;
@@ -24,6 +25,12 @@ namespace ERP.Backend.Application.Mapping
             CreateMap<UpdateCommandProduct, Product>().ForMember(member => member.Type, options => options.MapFrom(p => ProductTypeEnum.FromValue(p.TypeValue)));
             CreateMap<CreateRecipeDetailCommand, RecipeDetail>();
             CreateMap<UpdateRecipeDetailCommand, RecipeDetail>();
+            CreateMap<CreateOrderCommand, Order>().ForMember(member => member.Details, options => options.MapFrom(p => p.Details.Select(s => new OrderDetail
+            {
+                Price = s.Price,
+                ProductId = s.ProductId,
+                Quantity = s.Quantity
+            }).ToList()));
         }
     }
 }
