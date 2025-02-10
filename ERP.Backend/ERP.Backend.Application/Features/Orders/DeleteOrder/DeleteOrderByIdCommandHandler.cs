f91fdd32bfbd620d0c8435383d924dc45f3c4227
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-11 01:42:05 +0300

    feat: Implemented DeleteOrderByIdCommandHandler. - DeleteOrderByIdCommandHandler` was created and order delete function was added. - The order in the database was queried by `Id` and deleted. - A success message was returned when the deletion was completed successfully.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Orders/DeleteOrder/DeleteOrderByIdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Orders.DeleteOrder
+{
+    internal sealed class DeleteOrderByIdCommandHandler(IOrderReporsitory orderReporsitory, IUnitOfWork unitOfWork) : IRequestHandler<DeleteOrderByIdCommand, Result<string>>
+    {
+        public async Task<Result<string>> Handle(DeleteOrderByIdCommand request, CancellationToken cancellationToken)
+        {
+            Order order = await orderReporsitory.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+            if (order == null)
+            {
+                return Result<string>.Failure("Sipariş bulunamadı");
+            }
+            orderReporsitory.Delete(order);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return "Sipariş başarıyla silindi";
+        }
+    }
+}
