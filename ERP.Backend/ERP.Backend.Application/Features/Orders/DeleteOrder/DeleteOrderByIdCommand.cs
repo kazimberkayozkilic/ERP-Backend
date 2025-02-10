ff9f9f3ac5e2985f2d70ee78137a8e3fb61b771d
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-11 01:32:34 +0300

    fix: Changed the name of DeleteOrderByIdCommandcs to DeleteOrderByIdCommand. - The name `DeleteOrderByIdCommandcs` has been corrected to `DeleteOrderByIdCommand`.

--- ERP.Backend/ERP.Backend.Application/Features/Orders/DeleteOrder/DeleteOrderByIdCommand.cs
+++ ERP.Backend/ERP.Backend.Application/Features/Orders/DeleteOrder/DeleteOrderByIdCommand.cs
@@ -8,5 +8,5 @@ using TS.Result;
 
 namespace ERP.Backend.Application.Features.Orders.DeleteOrder
 {
-    public sealed record DeleteOrderByIdCommandcs(Guid Id): IRequest<Result<string>>;
+    public sealed record DeleteOrderByIdCommand(Guid Id): IRequest<Result<string>>;
 }
