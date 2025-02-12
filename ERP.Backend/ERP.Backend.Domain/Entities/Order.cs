0c93a707f37d673f4f781080a0ffd6cdeb18b079
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-13 01:54:26 +0300

    Feat: Added Order property and SetNumber method to create a formatted order number.

--- ERP.Backend/ERP.Backend.Domain/Entities/Order.cs
+++ ERP.Backend/ERP.Backend.Domain/Entities/Order.cs
@@ -2,6 +2,7 @@
 using ERP.Backend.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,23 @@ namespace ERP.Backend.Domain.Entities
     public sealed class Order : Entity
     {
         public Guid CustomerId { get; set; }
-        public Customer? Customer{ get; set; }
-        public int OrderNumberYear{ get; set; }
-        public int OrderNumber{ get; set; }
-        public DateOnly Date{ get; set; }
-        public DateOnly DeliveryDate{ get; set; }
+        public Customer? Customer { get; set; }
+        public int OrderNumberYear { get; set; }
+        public int OrderNumber { get; set; }
+        public string Number => SetNumber();
+        public DateOnly Date { get; set; }
+        public DateOnly DeliveryDate { get; set; }
         public OrderStatüsEnum Status { get; set; } = OrderStatüsEnum.Pending;
         public List<OrderDetail>? Details { get; set; }
+
+        public string SetNumber()
+        {
+            string prefix = "TS";
+            string initialString = prefix + OrderNumberYear.ToString() + OrderNumber.ToString();
+            int targerLength = 16;
+            int missingLength = targerLength - initialString.Length;
+            string finalString = prefix + OrderNumberYear.ToString() + new string('0', missingLength) + OrderNumber.ToString();
+            return finalString;
+        }
     }
 }
