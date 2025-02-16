ecaf957adc29be928ec84e8a8db75ddd959f518f
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-17 01:55:27 +0300

    chore: completed MG11 migration.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Migrations/20250216225454_mg11.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StockMovement",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DepotId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NumberOfEntries = table.Column<decimal>(type: "decimal(7,2)", nullable: false),
+                    NumberOfOutputs = table.Column<decimal>(type: "decimal(7,2)", nullable: false),
+                    Price = table.Column<decimal>(type: "money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockMovement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StockMovement_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockMovement_ProductId",
+                table: "StockMovement",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StockMovement");
+        }
+    }
+}
