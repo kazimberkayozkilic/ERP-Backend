70539339b89c1cdd21c7a2f40de672dface805cd
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-17 01:48:30 +0300

    feat: created IStockMovementRepository interface for StockMovement entity

---
+++ ERP.Backend/ERP.Backend.Domain/Repositories/IStockMovementRepository.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Backend.Domain.Entities;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Domain.Repositories
+{
+    public interface IStockMovementRepository : IRepository<StockMovement>
+    {
+    }
+}
