82d30917b1d911203814a32566bce880cef30f92
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-17 01:50:23 +0300

    feat: created StockMovementRepository implementing IStockMovementRepository

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/StockMovementRepository.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Repositories
+{
+    internal sealed class StockMovementRepository : Repository<StockMovement, ApplicationDbContext>, IStockMovementRepository
+    {
+        public StockMovementRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
