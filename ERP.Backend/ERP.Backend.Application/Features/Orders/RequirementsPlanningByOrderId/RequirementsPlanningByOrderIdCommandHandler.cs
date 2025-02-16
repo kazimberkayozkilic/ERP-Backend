462e1b06174073572cb1372d969a50cd954f8569
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-17 01:37:58 +0300

    refactor: implemented RequirementsPlanningByOrderIdCommandHandler with order retrieval logic.

--- ERP.Backend/ERP.Backend.Application/Features/Orders/RequirementsPlanningByOrderId/RequirementsPlanningByOrderIdCommandHandler.cs
+++ ERP.Backend/ERP.Backend.Application/Features/Orders/RequirementsPlanningByOrderId/RequirementsPlanningByOrderIdCommandHandler.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TS.Result;
 
 namespace ERP.Backend.Application.Features.Orders.RequirementsPlanningByOrderId
 {
-    internal class RequirementsPlanningByOrderIdCommandHandler
+    internal sealed class RequirementsPlanningByOrderIdCommandHandler(IOrderReporsitory orderReporsitory) : IRequestHandler<RequirementsPlanningByOrderIdCommand, Result<RequirementsPlanningByOrderIdCommandResponse>>
     {
+        public async Task<Result<RequirementsPlanningByOrderIdCommandResponse>> Handle(RequirementsPlanningByOrderIdCommand request, CancellationToken cancellationToken)
+        {
+            Order? order = await orderReporsitory.Where(p => p.Id == request.Id).Include(p => p.Details).FirstOrDefaultAsync(cancellationToken);
+
+            if (order is null)
+            {
+                return Result<RequirementsPlanningByOrderIdCommandResponse>.Failure("Sipariş bulunamadı");
+            }
+        }
     }
 }
