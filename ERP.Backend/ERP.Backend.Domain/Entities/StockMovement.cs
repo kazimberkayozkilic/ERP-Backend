1b0582e99a569a06f4786d0e488a64f9a45d1bf0
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-17 01:41:17 +0300

    feat: created StockMovement entity with properties for product, depot, entries, outputs, and price.

---
+++ ERP.Backend/ERP.Backend.Domain/Entities/StockMovement.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Backend.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Domain.Entities
+{
+    public sealed class StockMovement: Entity
+    {
+        public Guid ProductId { get; set; }
+        public Guid DepotId { get; set; }
+        public Product? Product { get; set; }
+        public decimal NumberOfEntries { get; set; }
+        public decimal NumberOfOutputs { get; set; }
+        public decimal Price { get; set; }
+    }
+}
