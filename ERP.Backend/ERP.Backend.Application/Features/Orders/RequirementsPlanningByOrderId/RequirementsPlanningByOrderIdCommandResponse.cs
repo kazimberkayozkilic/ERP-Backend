98049d7a6623019fcccf012d9bf8f255d6688695
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-16 23:18:28 +0300

    refactor: updated RequirementsPlanningByOrderIdCommandResponse to include Date, Title, and Products properties.

--- ERP.Backend/ERP.Backend.Application/Features/Orders/RequirementsPlanningByOrderId/RequirementsPlanningByOrderIdCommandResponse.cs
+++ ERP.Backend/ERP.Backend.Application/Features/Orders/RequirementsPlanningByOrderId/RequirementsPlanningByOrderIdCommandResponse.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using ERP.Backend.Domain.Dtos;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ERP.Backend.Application.Features.Orders.RequirementsPlanningByOrderId
 {
-    public sealed record RequirementsPlanningByOrderIdCommandResponse
-    {
-    }
+    public sealed record RequirementsPlanningByOrderIdCommandResponse(DateOnly Date,string Title,List<ProductDto> Products);
 }
