352ce7f15217c4366bea6aa6e0a8f2bb9da4485c
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-17 01:45:48 +0300

    feat: created StockMovementConfiguration with property type configurations.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Configurations/StockMomentConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using ERP.Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Configurations
+{
+    internal sealed class StockMomentConfiguration : IEntityTypeConfiguration<StockMovement>
+    {
+        public void Configure(EntityTypeBuilder<StockMovement> builder)
+        {
+            builder.Property(p => p.NumberOfEntries).HasColumnType("decimal(7,2)");
+            builder.Property(p => p.NumberOfOutputs).HasColumnType("decimal(7,2)");
+            builder.Property(p => p.Price).HasColumnType("money");
+
+        }
+    }
+}
