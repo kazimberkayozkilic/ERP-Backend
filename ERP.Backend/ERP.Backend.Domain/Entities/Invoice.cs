669ae77b277e920349515379f67f5579224daf3e
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-19 00:19:59 +0300

    feat: added InvoinceTypeEnum. - Created InvoinceTypeEnum using Ardalis.SmartEnum. - Added Purchase and Sales invoice types.

--- ERP.Backend/ERP.Backend.Domain/Entities/Invoice.cs
+++ ERP.Backend/ERP.Backend.Domain/Entities/Invoice.cs
@@ -1,10 +1,12 @@
 ﻿using ERP.Backend.Domain.Abstractions;
 using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Enums;
 
 public sealed class Invoice : Entity
 {
     public Guid CustomerId { get; set; }
     public Customer? Customer { get; set; }
     public string InvoiceNumber { get; set; } = string.Empty;
+    public InvoinceTypeEnum Type{ get; set; } = InvoinceTypeEnum.Purchase;
     public DateOnly Date { get; set; }
 }
---
+++ ERP.Backend/ERP.Backend.Domain/Enums/InvoinceTypeEnum.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.SmartEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Domain.Enums
+{
+    public sealed class InvoinceTypeEnum : SmartEnum<InvoinceTypeEnum>
+    {
+        public static readonly InvoinceTypeEnum Purchase = new("Alış Faturası", 1);
+        public static readonly InvoinceTypeEnum Sales = new("Satış Faturası", 2);
+
+        public InvoinceTypeEnum(string name, int value) : base(name, value)
+        {
+        }
+    }
+}
