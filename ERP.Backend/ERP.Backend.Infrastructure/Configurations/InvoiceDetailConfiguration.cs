cdec7af5b47245ae31c25e865b6812bd068e3d13
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-19 00:33:32 +0300

    feat: added OrderDetailConfiguration for EF Core. - Configured OrderDetail entity relationships. - Set DeleteBehavior to NoAction for Product reference. - Defined column types for Price and Quantity properties.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Configurations/InvoiceDetailConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+internal sealed class InvoiceDetailConfiguration : IEntityTypeConfiguration<InvoiceDetail>
+{
+    public void Configure(EntityTypeBuilder<InvoiceDetail> builder)
+    {
+        builder.HasOne(p => p.Product).WithMany().OnDelete(DeleteBehavior.NoAction);
+        builder.Property(p => p.Price).HasColumnType("money");
+        builder.Property(p => p.Quantity).HasColumnType("decimal(7,2)");
+    }
+}
