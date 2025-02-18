be6cccca0ff4d20e2898f073757c3171804ba0de
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-19 00:43:39 +0300

    feat: completed and updated mg12 migration. - Added Invoice relationship to StockMovement entity in the migration.

--- ERP.Backend/ERP.Backend.Domain/Repositories/IInvoiceRepository.cs
+++ ERP.Backend/ERP.Backend.Domain/Repositories/IInvoiceRepository.cs
@@ -1,4 +1,4 @@
-﻿,using GenericRepository;
+﻿using GenericRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ ERP.Backend/ERP.Backend.Infrastructure/Migrations/20250218214236_mg12.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "InvoiceId",
+                table: "StockMovement",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InvoiceNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvoicesDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InvoiceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<decimal>(type: "decimal(7,2)", nullable: false),
+                    Price = table.Column<decimal>(type: "money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvoicesDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InvoicesDetails_Invoices_InvoiceId",
+                        column: x => x.InvoiceId,
+                        principalTable: "Invoices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_InvoicesDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockMovement_InvoiceId",
+                table: "StockMovement",
+                column: "InvoiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_CustomerId",
+                table: "Invoices",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoicesDetails_InvoiceId",
+                table: "InvoicesDetails",
+                column: "InvoiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoicesDetails_ProductId",
+                table: "InvoicesDetails",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StockMovement_Invoices_InvoiceId",
+                table: "StockMovement",
+                column: "InvoiceId",
+                principalTable: "Invoices",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StockMovement_Invoices_InvoiceId",
+                table: "StockMovement");
+
+            migrationBuilder.DropTable(
+                name: "InvoicesDetails");
+
+            migrationBuilder.DropTable(
+                name: "Invoices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StockMovement_InvoiceId",
+                table: "StockMovement");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceId",
+                table: "StockMovement");
+        }
+    }
+}
