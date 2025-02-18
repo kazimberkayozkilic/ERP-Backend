614496045623fecf90d6faecb25629ffccb63681
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-19 00:16:29 +0300

    feat: added Invoice entity . - Created the Invoice entity with properties for customer, invoice number, and date .

---
+++ ERP.Backend/ERP.Backend.Domain/Entities/Invoice.cs
@@ -0,0 +1,10 @@
+﻿using ERP.Backend.Domain.Abstractions;
+using ERP.Backend.Domain.Entities;
+
+public sealed class Invoice : Entity
+{
+    public Guid CustomerId { get; set; }
+    public Customer? Customer { get; set; }
+    public string InvoiceNumber { get; set; } = string.Empty;
+    public DateOnly Date { get; set; }
+}
