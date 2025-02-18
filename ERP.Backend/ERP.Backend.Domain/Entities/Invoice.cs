3f3114d930d059feba0d3b61de60d6946aadda64
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-19 00:22:44 +0300

    feat: added InvoiceDetail entity. - Created InvoiceDetail entity with properties. - Linked to Invoice and Product entities.

--- ERP.Backend/ERP.Backend.Domain/Entities/Invoice.cs
+++ ERP.Backend/ERP.Backend.Domain/Entities/Invoice.cs
@@ -7,6 +7,7 @@ public sealed class Invoice : Entity
     public Guid CustomerId { get; set; }
     public Customer? Customer { get; set; }
     public string InvoiceNumber { get; set; } = string.Empty;
-    public InvoinceTypeEnum Type{ get; set; } = InvoinceTypeEnum.Purchase;
     public DateOnly Date { get; set; }
+    public InvoinceTypeEnum Type{ get; set; } = InvoinceTypeEnum.Purchase;
+    public List<InvoiceDetail>? Details{ get; set; }
 }
---
+++ ERP.Backend/ERP.Backend.Domain/Entities/InvoiceDetail.cs
@@ -0,0 +1,11 @@
+﻿using ERP.Backend.Domain.Abstractions;
+using ERP.Backend.Domain.Entities;
+
+public sealed class InvoiceDetail: Entity
+{
+    public Guid InvoiceId { get; set; }
+    public Guid ProductId { get; set; }
+    public Product? Product { get; set; }
+    public decimal Quantity { get; set; }
+    public decimal Price { get; set; }
+}
