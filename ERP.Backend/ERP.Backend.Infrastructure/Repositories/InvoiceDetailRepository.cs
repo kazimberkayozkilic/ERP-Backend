fd33ef7bfd266a90686a606f7b8d4196cb6d7e05
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-19 00:38:38 +0300

    feat: added InvoiceDetailRepository implementation. - Implemented InvoiceDetailRepository inheriting from Generic Repository. - Integrated ApplicationDbContext for database operations.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/InvoiceDetailRepository.cs
@@ -0,0 +1,10 @@
+﻿using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+
+internal sealed class InvoiceDetailRepository : Repository<InvoiceDetail, ApplicationDbContext>, IInvoiceDetailRepository
+{
+    public InvoiceDetailRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
