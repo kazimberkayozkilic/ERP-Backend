768088c7ff27256a483e69724687d1e0e2c306ee
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-20 02:07:29 +0300

    chore: applied migration mg13 for database schema updates.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Migrations/20250219230638_mg13.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepotId",
+                table: "InvoicesDetails",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoicesDetails_DepotId",
+                table: "InvoicesDetails",
+                column: "DepotId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvoicesDetails_Depots_DepotId",
+                table: "InvoicesDetails",
+                column: "DepotId",
+                principalTable: "Depots",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvoicesDetails_Depots_DepotId",
+                table: "InvoicesDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InvoicesDetails_DepotId",
+                table: "InvoicesDetails");
+
+            migrationBuilder.DropColumn(
+                name: "DepotId",
+                table: "InvoicesDetails");
+        }
+    }
+}
