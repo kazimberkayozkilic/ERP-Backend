fd7e0437be0ee53e4d37ff21ea93210f131153fc
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-20 01:00:57 +0300

    feat: added CreateOrderCommand for invoice creation.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Invoices/CreateInvoices/CreateInvoiceCommand.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Backend.Domain.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Invoices.CreateInvoices
+{
+    public sealed record class CreateOrderCommand(Guid CustomerId, DateOnly Date, string InvoiceNumber, List<InvoiceDetailDto> Details) : IRequest<Result<string>>;
+
+}
