5294943da8bdcb968cfdab3e816bdd962611ffc6
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-20 01:34:28 +0300

    feat: added UpdateInvoiceCommand for updating invoice details.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Invoices/UpdateInvoice/UpdateInvoiceCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Invoices.UpdateInvoice
+{
+    public sealed record UpdateInvoiceCommand(Guid Id, Guid CustomerId, int Type, DateOnly Date, string InvoiceNumber, List<InvoiceDetailDto> Details) : IRequest<Result<string>>;
+   
+}
