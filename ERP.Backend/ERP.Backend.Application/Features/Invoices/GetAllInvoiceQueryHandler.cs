caa27f6bdc3b589ee0ca84686053b1e4461368d0
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-20 00:45:45 +0300

    feat: implemented GetAllInvoiceQueryHandler to fetch invoices by type.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Invoices/GetAllInvoiceQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Backend.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Application.Features.Invoices
+{
+    internal sealed class GetAllInvoiceQueryHandler(IInvoiceRepository invoiceRepository) : IRequestHandler<GetAllInvoiceQuery, List<Invoice>>
+    {
+        public async Task<List<Invoice>> Handle(GetAllInvoiceQuery request, CancellationToken cancellationToken)
+        {
+            List<Invoice> invoices = await invoiceRepository.Where(p => p.Type.Value == request.Type).OrderBy(p => p.Date).ToListAsync(cancellationToken);
+            return invoices;
+        }
+    }
+}
