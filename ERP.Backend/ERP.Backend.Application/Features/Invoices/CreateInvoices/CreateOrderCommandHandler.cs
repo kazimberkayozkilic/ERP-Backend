cd363e49b0210d16de20f03fcda7078515eb5484
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-22 01:53:03 +0300

    fix: Replaced item.InvoiceId with invoice.Id in CreateOrderCommandHandler.

--- ERP.Backend/ERP.Backend.Application/Features/Invoices/CreateInvoices/CreateOrderCommandHandler.cs
+++ ERP.Backend/ERP.Backend.Application/Features/Invoices/CreateInvoices/CreateOrderCommandHandler.cs
@@ -18,7 +18,7 @@ internal sealed class CreateOrderCommandHandler(IInvoiceRepository invoiceReposi
             {
                 StockMovement movement = new()
                 {
-                    InvoiceId = item.InvoiceId,
+                    InvoiceId = invoice.Id,
                     NumberOfEntries = request.TypeValue == 1 ? item.Quantity : 0,
                     NumberOfOutputs = request.TypeValue == 2 ? item.Quantity : 0,
                     DepotId = item.DepotId,
