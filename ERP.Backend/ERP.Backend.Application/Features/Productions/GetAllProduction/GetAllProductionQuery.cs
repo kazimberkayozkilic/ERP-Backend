a56bc7204802b5653e808ee0389739077470a7f7
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-23 02:02:29 +0300

    feat: Added GetAllProductionQuery for retrieving all productions.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Productions/GetAllProduction/GetAllProductionQuery.cs
@@ -0,0 +1,13 @@
+﻿using ERP.Backend.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Productions.GetAllProduction
+{
+    public sealed record GetAllProductionQuery():IRequest<Result<List<Production>>>;
+}
