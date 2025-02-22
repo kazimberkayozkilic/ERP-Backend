f5b0bffbc7e2e5927e4bafea5d6a59c63448a445
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-23 01:26:00 +0300

    feat: Added Production entity with properties for product and quantity tracking.

---
+++ ERP.Backend/ERP.Backend.Domain/Entities/Production.cs
@@ -0,0 +1,17 @@
+﻿using ERP.Backend.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Domain.Entities
+{
+    public sealed class Production: Entity
+    {
+        public Guid ProductId { get; set; }
+        public Product? Product { get; set; }
+        public decimal Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
