a80494050326f012bd7e45251db7ddeae7e610f9
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-23 01:46:20 +0300

    feat: Added ProductionConfiguration with decimal type for Quantity property.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Configurations/ProductionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Configurations
+{
+    internal sealed class ProductionConfiguration : IEntityTypeConfiguration<Production>
+    {
+        public void Configure(EntityTypeBuilder<Production> builder)
+        {
+            builder.Property(p => p.Quantity).HasColumnType("decimal(7,2)");
+        }
+    }
+}
