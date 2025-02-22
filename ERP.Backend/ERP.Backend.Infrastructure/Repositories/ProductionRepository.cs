ed41766f285f93c4305479ed56bc1487a272f986
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-23 01:44:03 +0300

    feat: Implemented ProductionRepository with ApplicationDbContext.

---
+++ ERP.Backend/ERP.Backend.Infrastructure/Repositories/ProductionRepository.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using ERP.Backend.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Infrastructure.Repositories
+{
+    internal sealed class ProductionRepository : Repository<Production, ApplicationDbContext>, IProductionRepository
+    {
+        public ProductionRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
