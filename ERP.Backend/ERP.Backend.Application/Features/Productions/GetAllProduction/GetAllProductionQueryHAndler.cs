a51fec776505dc370c41bba5d7b5f32a11f596ff
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-23 02:17:21 +0300

    feat: Implemented GetAllProductionQueryHandler to handle production retrieval.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Productions/GetAllProduction/GetAllProductionQueryHAndler.cs
@@ -0,0 +1,22 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Productions.GetAllProduction
+{
+    internal sealed class GetAllProductionQueryHAndler(IProductionRepository  productionRepository) : IRequestHandler<GetAllProductionQuery, Result<List<Production>>>
+    {
+        public async Task<Result<List<Production>>> Handle(GetAllProductionQuery request, CancellationToken cancellationToken)
+        {
+            List<Production> productions = await productionRepository.GetAll().OrderByDescending(p => p.CreatedAt).ToListAsync(cancellationToken);
+            return productions;
+        }
+    }
+}
