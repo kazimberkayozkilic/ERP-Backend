c9fc4f102ed374aa462612cce7158a16b3f5bb78
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-24 02:01:13 +0300

    feat: Added DeleteProductionByIdCommand for deleting a production entry.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Productions/DeleteProductionById/DeleteProductionByIdCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Productions.DeleteProductionById
+{
+    public sealed record DeleteProductionByIdCommand(Guid Id) : IRequest<Result<string>>;
+    
+}
