656816866f39a7ebed05428f3d0c40f49ae85962
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-24 02:07:44 +0300

    feat: Implemented DeleteProductionByIdCommandHandler for handling production deletion.

---
+++ ERP.Backend/ERP.Backend.Application/Features/Productions/DeleteProductionById/DeleteProductionByIdCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using ERP.Backend.Domain.Entities;
+using ERP.Backend.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERP.Backend.Application.Features.Productions.DeleteProductionById
+{
+    internal sealed class DeleteProductionByIdCommandHandler(
+       IProductionRepository productionRepository,
+       IStockMovementRepository stockMovementRepository,
+       IUnitOfWork unitOfWork) : IRequestHandler<DeleteProductionByIdCommand, Result<string>>
+    {
+        public async Task<Result<string>> Handle(DeleteProductionByIdCommand request, CancellationToken cancellationToken)
+        {
+            Production? production = await productionRepository.GetByExpressionWithTrackingAsync(p => p.Id == request.Id, cancellationToken);
+
+            if (production is null)
+            {
+                return Result<string>.Failure("Üretim bulunamadı");
+            }
+
+            List<StockMovement> movements = await stockMovementRepository.Where(p => p.ProductionId == production.Id).ToListAsync(cancellationToken);
+
+            stockMovementRepository.DeleteRange(movements);
+
+            productionRepository.Delete(production);
+
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return "Üretim başarıyla silindi";
+        }
+    }
+}
