46f839086eceeaa943fe5bdea22f5b2ba8b53e5e
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-26 00:39:56 +0300

    feat: Added optional OrderId parameter to CreateInvoiceCommand.

--- ERP.Backend/ERP.Backend.Application/Features/Invoices/CreateInvoices/CreateInvoiceCommand.cs
+++ ERP.Backend/ERP.Backend.Application/Features/Invoices/CreateInvoices/CreateInvoiceCommand.cs
@@ -10,6 +10,6 @@ using TS.Result;
 
 namespace ERP.Backend.Application.Features.Invoices.CreateInvoices
 {
-    public sealed record class CreateInvoiceCommand(Guid CustomerId, int TypeValue, DateOnly Date, string InvoiceNumber, List<InvoiceDetailDto> Details) : IRequest<Result<string>>;
+    public sealed record class CreateInvoiceCommand(Guid CustomerId, int TypeValue, DateOnly Date, string InvoiceNumber, List<InvoiceDetailDto> Details, Guid? OrderId) : IRequest<Result<string>>;
 
 }
