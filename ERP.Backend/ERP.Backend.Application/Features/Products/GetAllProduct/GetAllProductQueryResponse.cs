6fb9760c0aca599e0af4fbcb224d3b7f8bb9523d
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-02-27 01:24:57 +0300

    fix: Changed 'stock' property to 'Stock' in GetAllProductQueryResponse.

--- ERP.Backend/ERP.Backend.Application/Features/Products/GetAllProduct/GetAllProductQueryResponse.cs
+++ ERP.Backend/ERP.Backend.Application/Features/Products/GetAllProduct/GetAllProductQueryResponse.cs
@@ -7,7 +7,7 @@ namespace ERP.Backend.Application.Features.Products.GetAllProduct
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public ProductTypeEnum Type { get; set; } = ProductTypeEnum.Product;
-        public decimal stock { get; set; }
+        public decimal Stock { get; set; }
     }
 
 
